b291ce3ae22563adce027b6167b08b9f3b616c9e
RAVINTHAR S <104417627+Ravinthar007@users.noreply.github.com>
2024-10-18 17:52:59 +0530

    Add files via upload

---
+++ Maximum arrays/maxarray.py
@@ -0,0 +1,9 @@
+arr1 = int(input("Enter the array 1: "))
+arr2 = int(input("Enter the array 2: "))
+arr3 = int(input("Enter the array 3: "))
+arr4 = int(input("Enter the array 4: "))
+arr5 = int(input("Enter the array 5: "))
+max_element = max(arr1,arr2,arr3,arr4,arr5)
+print("The maximum element in the array is:", max_element)
+
+input("Press Enter to exit...")
